8bfce432b20ac4f4447019b24fdc37657c3811d3
Pawan Kumar <97224451+Pawantechofficial@users.noreply.github.com>
2024-02-01 14:11:17 +0530

    Update Footer.jsx
    justify

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaArrowCircleUp } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-center sm:justify-between">
+      <div className="container p-12 flex justify-evenly sm:justify-between">
         <span className="hidden sm:block">LOGO</span>
         <p className="text-slate-200">All rights reserved.</p>
         <div>
